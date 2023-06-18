f58f4814b88373e1c3904d0bd21d9f2dcf15bf0b
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-06-18 11:00:53 +0400

    Update RecipeService

--- src/main/java/com/API/RecipeManagement/Services/RecipeService.java
+++ src/main/java/com/API/RecipeManagement/Services/RecipeService.java
@@ -1,9 +1,22 @@
 package com.API.RecipeManagement.Services;
 
 
+import com.API.RecipeManagement.Models.RecipesModel;
+import com.API.RecipeManagement.Repository.RecipeRepo;
+import com.API.RecipeManagement.RequestObjects.RecipeRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RecipeService {
 
+    @Autowired
+    RecipeRepo  recipeRepo;
+
+    public String savingRecipe(RecipeRequest recipeRequest){
+        RecipesModel newRecipe = new RecipesModel();
+        newRecipe= RecipeRequest.convertToEntity(newRecipe,recipeRequest);
+        return  "Successfully Saved The Recipe";
+
+    }
 }

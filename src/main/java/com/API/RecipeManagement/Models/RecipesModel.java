1341b82008daa44b0f38157d14cca1aa464308b7
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-06-18 10:18:03 +0400

    Create a RecipesModel Class.

---
+++ src/main/java/com/API/RecipeManagement/Models/RecipesModel.java
@@ -0,0 +1,25 @@
+package com.API.RecipeManagement.Models;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.List;
+
+@Data
+@Entity
+public class RecipesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    Integer id;
+    String recipeName;
+    List<String> ingredient;
+    String instruction;
+    Integer cookingTime;
+
+
+}

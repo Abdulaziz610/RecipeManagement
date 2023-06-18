50505e0792cb04d79fe82169a06a0a5e177e9d26
Abdulaziz610 <Abdulaziz.tech@gmail.com>
2023-06-18 11:08:31 +0400

    Create RecipeController.

--- src/main/java/com/API/RecipeManagement/Controllers/RecipeController.java
+++ src/main/java/com/API/RecipeManagement/Controllers/RecipeController.java
@@ -1,11 +1,16 @@
 package com.API.RecipeManagement.Controllers;
 
 
+import com.API.RecipeManagement.Services.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "recipe")
 public class RecipeController {
+    @Autowired
+    RecipeService recipeService;
+
 
 }
